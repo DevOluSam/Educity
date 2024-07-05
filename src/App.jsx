0786332c729e740463587d9cdd70149eee7b4550
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-07-05 13:22:11 +0100

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -20,6 +22,9 @@ function App() {
       <Campus />
       <Title subTitle='TESTIMONIALS' title='What Student Says'/>
       <Testimonials />
+      <Title subTitle='Contact Us' title='Get in Touch'/>
+      <Contact />
+      <Footer />
       </div>
     </div>
   )
